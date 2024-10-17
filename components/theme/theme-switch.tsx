a5639b614f5362596fbb6002247885ffbeacf57b
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2024-10-17 19:50:46 +0530

    update Theme Switch

--- components/theme/theme-switch.tsx
+++ components/theme/theme-switch.tsx
@@ -31,9 +31,9 @@ const ThemeSwitch = () => {
 
     return (
         <div className='h-[41px] flex items-center px-[6px] py-[4px] rounded-full border border-imcrox_border_primary w-fit'>
-            <div onClick={() => handleChangeTheme({t:"light"})} className={`w-[28px] h-[28px] rounded-full flex justify-center items-center cursor-pointer ${theme === "light" ? "bg-secondary-50" : ""} `}><Sun size={16} /></div>
-            <div onClick={() =>handleChangeTheme({t:"system"})} className={`w-[28px] h-[28px] rounded-full flex justify-center items-center cursor-pointer ${theme === "system" ? "bg-secondary-50" : ""} `}><Monitor size={16} /></div>
-            <div onClick={() => handleChangeTheme({t:"dark"})} className={`w-[28px] h-[28px] rounded-full flex justify-center items-center cursor-pointer ${theme === "dark" ? "bg-secondary-50" : ""} `}><Moon size={16} /></div>
+            <div onClick={() => handleChangeTheme({t:"light"})} className={`w-[28px] h-[28px] rounded-full flex justify-center items-center cursor-pointer ${theme === "light" ? "dark:bg-[#1A1A1A] bg-[#f7f7f7]" : ""} `}><Sun size={16} /></div>
+            <div onClick={() =>handleChangeTheme({t:"system"})} className={`w-[28px] h-[28px] rounded-full flex justify-center items-center cursor-pointer ${theme === "system" ? "dark:bg-[#1A1A1A] bg-[#f7f7f7]" : ""} `}><Monitor size={16} /></div>
+            <div onClick={() => handleChangeTheme({t:"dark"})} className={`w-[28px] h-[28px] rounded-full flex justify-center items-center cursor-pointer ${theme === "dark" ? "dark:bg-[#1A1A1A] bg-[#f7f7f7]" : ""} `}><Moon size={16} /></div>
         </div>
     )
 }
